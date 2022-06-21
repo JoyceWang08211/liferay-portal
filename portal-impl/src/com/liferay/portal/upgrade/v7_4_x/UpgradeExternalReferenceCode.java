03f871ac2a80747abd28d4ec46c3b8109a2dff58
Joyce Wang <joyce.wang@liferay.com>
2022-06-21 17:19:35 +0800

    Revert "LPS-155786 Add logging"
    This reverts commit 1ff3717415a162aa344fc4735155d4d3bf62916b.

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeExternalReferenceCode.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeExternalReferenceCode.java
@@ -15,8 +15,6 @@
 package com.liferay.portal.upgrade.v7_4_x;
 
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -83,15 +81,7 @@ public class UpgradeExternalReferenceCode extends UpgradeProcess {
 		throws Exception {
 
 		if (!hasTable(tableName)) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Skip nonexistent table " + tableName);
-			}
-
 			return;
-		}
-
-		if (_log.isWarnEnabled()) {
-			_log.warn("Upgrade table " + tableName);
 		}
 
 		if (!hasColumn(tableName, "externalReferenceCode")) {
@@ -153,8 +143,5 @@ public class UpgradeExternalReferenceCode extends UpgradeProcess {
 			}
 		}
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		UpgradeExternalReferenceCode.class);
 
 }
