79b19361c2fff82c8fbf2a253c095eb900eb179b
Joyce Wang <joyce.wang@liferay.com>
2022-07-26 10:02:32 +0800

    Revert "LPS-158528 SF"
    This reverts commit 7986dc7ca48332f481d00b0641120d78988617a4.

--- modules/apps/portal-remote/portal-remote-jaxrs-whiteboard/src/main/java/com/liferay/portal/remote/jaxrs/whiteboard/internal/servlet/filter/JAXRSActivationFilterTracker.java
+++ modules/apps/portal-remote/portal-remote-jaxrs-whiteboard/src/main/java/com/liferay/portal/remote/jaxrs/whiteboard/internal/servlet/filter/JAXRSActivationFilterTracker.java
@@ -78,7 +78,7 @@ public class JAXRSActivationFilterTracker {
 			_readyServiceRegistration = _bundleContext.registerService(
 				Object.class, new Object(),
 				MapUtil.singletonDictionary(
-					"liferay.jaxrs.whiteboard.ready", true));
+					_LIFERAY_JAXRS_WHITEBOARD_READY, true));
 
 			_unregister();
 		}
@@ -91,6 +91,9 @@ public class JAXRSActivationFilterTracker {
 			_filterServiceRegistration = null;
 		}
 	}
+
+	private static final String _LIFERAY_JAXRS_WHITEBOARD_READY =
+		"liferay.jaxrs.whiteboard.ready";
 
 	private BundleContext _bundleContext;
 	private CountDownLatch _countDownLatch;
