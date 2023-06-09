48b197ff5274041e576ae652f3cc2cfe6dd4dd1c
JoyceWang08211 <joyce.wang@liferay.com>
2023-06-09 15:31:18 +0800

    LRQA-80963 TF

--- modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/util/JSONCurlUtil.java
+++ modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/util/JSONCurlUtil.java
@@ -71,6 +71,14 @@ public class JSONCurlUtil {
 		return _getParsedResponse(request, jsonPath);
 	}
 
+	public static String head(String requestString)
+		throws IOException, TimeoutException {
+
+		Request request = new Request(requestString, "HEAD");
+
+		return request.send();
+	}
+
 	public static String patch(String requestString)
 		throws IOException, TimeoutException {
 
